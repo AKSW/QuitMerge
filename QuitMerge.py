bcb77b88d3bdb6e13be013173beabc4783246e68
Natanael Arndt <arndtn@gmail.com>
2016-10-12 18:40:35 +0200

    Add delta method

--- QuitMerge.py
+++ QuitMerge.py
@@ -26,28 +26,52 @@ class QuitMerge:
         print ("diff O:A")
         fO = open(base, 'r')
         fA = open(local, 'r')
-        # use list() instead of readlines()
-        # https://stupidpythonideas.blogspot.de/2013/06/readlines-considered-silly.html
-        OA = difflib.ndiff(sorted(list(fO)), sorted(list(fA)))
-        print(OA)
-        sys.stdout.writelines(OA)
-        for line in OA:
-            print(line)
+        delta = self.delta(fO, fA)
         fO.close()
         fA.close()
+
+        print ("set")
+        print ("add")
+        for line in delta["add"]:
+            print(line)
+        print ("delete")
+        for line in delta["delete"]:
+            print(line)
+
 
         print ("diff O:B")
 
         fO = open(base, 'r')
         fB = open(remote, 'r')
-        OB = difflib.ndiff(sorted(list(fO)), sorted(list(fB)))
-        print(OB)
-        sys.stdout.writelines(OB)
-        for line in OB:
-            print(line)
+        delta = self.delta(fO, fB)
         fO.close()
         fB.close()
+
+        print ("set")
+        print ("add")
+        for line in delta["add"]:
+            print(line)
+        print ("delete")
+        for line in delta["delete"]:
+            print(line)
 
         # merge result has to be written to *local*
 
         return
+
+    def delta(self, local, remote):
+        # use list() instead of readlines()
+        # https://stupidpythonideas.blogspot.de/2013/06/readlines-considered-silly.html
+        lineDelta = difflib.ndiff(sorted(list(local)), sorted(list(remote)))
+        print(lineDelta)
+
+        addSet = []
+        deleteSet = []
+
+        for line in lineDelta:
+            if line.startswith('+ '):
+                addSet.append(line[2:])
+            if line.startswith('- '):
+                deleteSet.append(line[2:])
+
+        return {"add": addSet, "delete": deleteSet}
